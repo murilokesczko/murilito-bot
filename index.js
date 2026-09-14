d33cab42f18568ce98b343f1388c862474d08bc5
murilokesczko <murilokesczko@gmail.com>
2026-09-13 21:38:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,26 @@ const client = new Client({
 client.once('ready', () => {
   console.log(`✅ Bot conectado como ${client.user.tag}`);
 
-  // Canal onde o Murilito vai postar
-  const canal = client.channels.cache.get("1520508956978712576");
+  // Canal onde o Murilo NEWS vai postar
+  const canal = client.channels.cache.get("1517339263216390164");
 
   // Mensagem de teste ao iniciar
   if (canal) {
-    canal.send("@everyone ✅ Teste: Murilito está funcionando e pronto pra postar notícias!");
+    canal.send("@everyone ✅ Teste: Murilo NEWS está funcionando e pronto pra postar notícias!");
   } else {
     console.error("❌ Canal não encontrado. Verifique o ID.");
+  }
+
+  // Função para postar notícias do Fortnite
+  async function postarFortnite() {
+    try {
+      const feed = await parser.parseURL('https://fortnite.gg/news/rss');
+      const noticia = feed.items[0];
+      canal.send(`@everyone 📰 **Nova notícia de Fortnite: E use o código na Loja: TIOKHREBIS**\n${noticia.title}\n${noticia.link}`);
+      console.log(`Fortnite postada: ${noticia.title}`);
+    } catch (err) {
+      console.error('Erro ao buscar Fortnite:', err);
+    }
   }
 
   // Função para postar notícias do LibertyCity
@@ -48,7 +60,8 @@ client.once('ready', () => {
     }
   }
 
-  // Checar ambos a cada 10 minutos
+  // Checar todos os sites a cada 10 minutos
+  setInterval(postarFortnite, 600000);
   setInterval(postarLibertyCity, 600000);
   setInterval(postarRockstar, 600000);
 });
